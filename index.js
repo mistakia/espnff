2c3ebc9e656e0c4cb9cffc8dee5b88623c82095c
t3rr0r <mail@t3rr0r.com>
2017-09-08 15:30:24 -0400

    added schedule and standings parsing

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const get_activity = require('./lib/activity')
+const standings = require('./lib/standings')
+const schedule = require('./lib/schedule')
 
 module.exports = {
-  get_activity: get_activity
+  get_activity: get_activity,
+  standings: standings,
+  schedule: schedule
 }
