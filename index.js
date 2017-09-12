7e99adf840f4776b1ab262823dfd18960fa10ea8
t3rr0r <mail@t3rr0r.com>
2017-09-12 10:59:59 -0400

    fetch team rosters

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const get_activity = require('./lib/activity')
 const standings = require('./lib/standings')
 const schedule = require('./lib/schedule')
+const roster = require('./lib/roster')
 
 module.exports = {
   get_activity: get_activity,
   standings: standings,
-  schedule: schedule
+  schedule: schedule,
+  roster: roster
 }
