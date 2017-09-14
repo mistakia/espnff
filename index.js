6fd4e7c53d8fc907a22429e367160561d939d512
t3rr0r <mail@t3rr0r.com>
2017-09-13 21:40:09 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const get_activity = require('./lib/activity')
+const activity = require('./lib/activity')
 const standings = require('./lib/standings')
 const schedule = require('./lib/schedule')
 const roster = require('./lib/roster')
 
 module.exports = {
-  get_activity: get_activity,
+  activity: activity,
   standings: standings,
   schedule: schedule,
   roster: roster
