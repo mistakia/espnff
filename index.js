2940c1bab5b6d0bcb46697c99c0fca9ea7c33a91
t3rr0r <mail@t3rr0r.com>
2017-09-23 17:58:34 -0400

    added boxscore

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const activity = require('./lib/activity')
 const standings = require('./lib/standings')
 const schedule = require('./lib/schedule')
 const roster = require('./lib/roster')
+const boxscore = require('./lib/boxscore')
 
 module.exports = {
   activity: activity,
   standings: standings,
   schedule: schedule,
-  roster: roster
+  roster: roster,
+  boxscore: boxscore
 }
