a1087372248059768c8064b6add3b27975c754c4
t3rr0r <mail@t3rr0r.com>
2017-09-28 17:20:09 -0400

    get draft results

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const activity = require('./lib/activity')
 const standings = require('./lib/standings')
 const schedule = require('./lib/schedule')
+const draft = require('./lib/draft')
 const roster = require('./lib/roster')
 const boxscore = require('./lib/boxscore')
 
 module.exports = {
   activity: activity,
   standings: standings,
+  draft: draft,
   schedule: schedule,
   roster: roster,
   boxscore: boxscore
